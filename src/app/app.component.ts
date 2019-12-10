c079f57634835f55a8b2c2be8f98f056173de627
Fihoca05 <Fihoca05@SINENSIA.EDU>
2019-12-10 16:38:02 +0100

    first commi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'primenghw';
+export class AppComponent implements OnInit{
+  
+  cars:any[] = [];
+  date1:Date = new Date();
+  text: string;
+  selectedCities: string[] = [];
+  val: number = 2;
+  temperatura: number = 10;
+  selected: string = "";
+  
+  ngOnInit(): void {
+    this.cars.push({codigo: "45344", empresa: "ADEPSA", codigoPostal: "08020", email: "info@adepsa.com"});
+    this.cars.push({codigo: "98245", empresa: "CIRCOSA", codigoPostal: "08445", email: "admin@circosa.es"});
+    this.cars.push({codigo: "23439", empresa: "Comercial Millo, S.L.", codigoPostal: "68022", email: "p.damaso@millo.com"});
+    this.cars.push({codigo: "10933", empresa: "Ferretería Hermanos Matanzas", codigoPostal: "08012", email: "info@technomat.com"});
+    this.cars.push({codigo: "10552", empresa: "Central de la Gafa Distribución S.A.", codigoPostal: "08044", email: "admin@cgd.com" });
+    this.cars.push({codigo: "48924", empresa: "DIBUG S.A.", codigoPostal: "25912", email: "dibug@dibug.net"});
+    this.cars.push({codigo: "29233", empresa: "Aceral S.L.", codigoPostal: "08072", email: "carlos.moreno544@gmail.com"});
+    this.cars.push({codigo: "31562", empresa: "CIM S.A.", codigoPostal: "08020", email: "info@cim.com"});
+    this.cars.push({codigo: "48992", empresa: "Ramón Batlle", codigoPostal: "08013", email: "r.batlle@gmail.com"});
+    this.cars.push({codigo: "10935", empresa: "SOFyGEST, S.L.", codigoPostal: "08003", email: "c.vilagut@sofygest.com"});
+
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule }                 from '@angular/core';
 
-import { AppComponent } from './app.component';
+// Moduls
+import { FormsModule }              from '@angular/forms';
+import { BrowserModule }            from '@angular/platform-browser';
+import { BrowserAnimationsModule }  from '@angular/platform-browser/animations'
+import { ButtonModule }             from 'primeng/button';
+import { TableModule }              from 'primeng/table';
+import { CardModule }               from 'primeng/card';
+import { CalendarModule }           from 'primeng/calendar';
+import { CheckboxModule }           from 'primeng/checkbox';
+import { RatingModule }             from 'primeng/rating';
+import { SpinnerModule }            from 'primeng/spinner';
+import { DropdownModule }           from 'primeng/dropdown';
+import { TooltipModule }            from 'primeng/tooltip';
+
+// Components
+import { AppComponent }             from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    TableModule,
+    CardModule,
+    CalendarModule,
+    CheckboxModule,
+    RatingModule,
+    SpinnerModule,
+    DropdownModule,
+    TooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
